b952ee225c4ee1a112e155af8b8f7e34d47f7cc4
gleb.lobastov <gleb.lobastov@gmail.com>
2019-02-10 12:34:40 +0300

    feat(ExchangerPage): render ExchangeRateMonitor and PocketSwapper

--- src/pages/ExchangerPage/ExchangerPage.jsx
+++ src/pages/ExchangerPage/ExchangerPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import ExchangingPocket from './blocks/ExchangingPocket';
+import ExchangeRateMonitor from './blocks/ExchangeRateMonitor';
+import PocketsSwapper from './blocks/PocketsSwapper';
 
 const ExchangerPage = () => (
   <div data-locator="exchanger-pocket-selector">
+    <ExchangeRateMonitor
+      creditCurrencyCode="USD"
+      debitCurrencyCode="EUR"
+      exchangeRate={0.88124}
+    />
     <ExchangingPocket />
+    <PocketsSwapper />
     <ExchangingPocket />
     <div data-locator="exchanger-action-button">
       <Button>Exchange</Button>
